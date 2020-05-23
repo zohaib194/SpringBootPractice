d2c0ea659f92ba2ec53dab80a003ad0a3af3aed2
zohaib <zohaib_b13@hotmail.com>
2020-05-23 14:15:12 +0200

    Added hello world example

---
+++ spring.example/src/main/java/com/exercise/spring/example/Controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.exercise.spring.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String root(){
+        return "Hello World";
+    }
+}
--- spring.example/src/main/java/com/exercise/spring/example/controller/HelloWorldController.java
+++
@@ -1,4 +0,0 @@
-package com.exercise.spring.example.controller;
-
-public class HelloWorldController {
-}
